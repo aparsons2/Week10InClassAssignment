65a176306e1d2c484cd26a16caace6ac6bcb7430
ashlyn <aparsons2@colum.edu>
2021-04-05 19:39:26 -0500

    INCLASSUPDATE1

--- Week10InClassAssignment/Assets/Scripts/CameraMgr.cs
+++ Week10InClassAssignment/Assets/Scripts/CameraMgr.cs
@@ -21,7 +21,19 @@ public class CameraMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            Vector3 diff = Vector3.forward * Input.mouseScrollDelta.y;
+
+            if (mainCamera.enabled)
+                mainCamera.transform.Translate(diff);
+
+            else if (SideViewCamera.enabled)
+                SideViewCamera.transform.Translate(diff);
+
+            else if (SplitScreenLower.enabled)
+                SplitScreenLower.transform.Translate(diff);
+        }
     }
     public void OnSliderValueChanged(float value)
     {
